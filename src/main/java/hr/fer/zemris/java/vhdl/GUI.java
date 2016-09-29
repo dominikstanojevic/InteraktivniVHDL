cfa9a618ac910bcee82846e5da7eee9177377455
dominikstanojevic <dominik.stanojevic@outlook.com>
2016-09-29 09:20:56 +0200

    Pane now zooms in/out according to the mouse position.

--- src/main/java/hr/fer/zemris/java/vhdl/GUI.java
+++ src/main/java/hr/fer/zemris/java/vhdl/GUI.java
@@ -8,22 +8,27 @@ import hr.fer.zemris.java.vhdl.models.values.LogicValue;
 import hr.fer.zemris.java.vhdl.models.values.Value;
 import hr.fer.zemris.java.vhdl.models.values.Vector;
 
+import javax.swing.AbstractAction;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLayer;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
 import javax.swing.plaf.LayerUI;
-import java.awt.AWTEvent;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.MouseInfo;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseWheelEvent;
 import java.util.Optional;
 
 /**
@@ -33,6 +38,7 @@ public class GUI extends JFrame implements IModelListener {
 	private Model model;
 	private VHDLComponent blackBox;
 	private JLayer<JComponent> layer;
+	private JScrollPane scrollPane;
 
 	public GUI(Model model) {
 		this.model = model;
@@ -52,10 +58,12 @@ public class GUI extends JFrame implements IModelListener {
 		JPanel panel = new JPanel(new BorderLayout());
 		ZoomUI zoomUI = new ZoomUI();
 		layer = new JLayer<>(blackBox, zoomUI);
+		scrollPane = new JScrollPane(layer);
 
-		panel.add(new JScrollPane(layer), BorderLayout.CENTER);
+		panel.add(scrollPane, BorderLayout.CENTER);
 		cp.add(panel);
 		addMouseListener(zoomUI);
+		addKeyBindings(zoomUI);
 	}
 
 	private void addMouseListener(ZoomUI zoomUI) {
@@ -92,22 +100,22 @@ public class GUI extends JFrame implements IModelListener {
 				layer.repaint();
 			}
 		});
+	}
 
-		layer.addMouseWheelListener(new MouseAdapter() {
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				if (e.getWheelRotation() == 1) {
-					zoomUI.zoomIn();
-				} else {
-					zoomUI.zoomOut();
-				}
+	private void addKeyBindings(ZoomUI zoomUI) {
+		layer.getInputMap()
+				.put(KeyStroke.getKeyStroke(KeyEvent.VK_ADD, InputEvent.CTRL_MASK), "zoomIn");
+		layer.getInputMap()
+				.put(KeyStroke.getKeyStroke(KeyEvent.VK_PLUS, InputEvent.CTRL_MASK), "zoomIn");
+		layer.getActionMap().put("zoomIn", zoomUI.zoomIn);
 
-				layer.setPreferredSize(new Dimension((int) (zoomUI.zoom * blackBox.getWidth()),
-						(int) (zoomUI.zoom * blackBox.getHeight())));
-				layer.revalidate();
-				layer.repaint();
-			}
-		});
+		layer.getInputMap()
+				.put(KeyStroke.getKeyStroke(KeyEvent.VK_MINUS, InputEvent.CTRL_MASK),
+						"zoomOut");
+		layer.getInputMap()
+				.put(KeyStroke.getKeyStroke(KeyEvent.VK_SUBTRACT, InputEvent.CTRL_MASK),
+						"zoomOut");
+		layer.getActionMap().put("zoomOut", zoomUI.zoomOut);
 	}
 
 	@Override
@@ -115,23 +123,9 @@ public class GUI extends JFrame implements IModelListener {
 		blackBox.repaint();
 	}
 
-	private static class ZoomUI extends LayerUI<JComponent> {
+	private class ZoomUI extends LayerUI<JComponent> {
 		private double zoom = 1;
 		public static final double INCREMENT = 0.1;
-
-		public void installUI(JComponent c) {
-			super.installUI(c);
-			JLayer jlayer = (JLayer) c;
-			jlayer.setLayerEventMask(
-					AWTEvent.MOUSE_EVENT_MASK | AWTEvent.MOUSE_WHEEL_EVENT_MASK);
-		}
-
-		@Override
-		public void uninstallUI(JComponent c) {
-			JLayer jlayer = (JLayer) c;
-			jlayer.setLayerEventMask(0);
-			super.uninstallUI(c);
-		}
 
 		@Override
 		public void paint(Graphics g, JComponent c) {
@@ -139,34 +133,59 @@ public class GUI extends JFrame implements IModelListener {
 
 			g2.scale(zoom, zoom);
 			super.paint(g2, c);
-
 			g2.dispose();
 		}
 
-		@Override
-		protected void processMouseWheelEvent(
-				MouseWheelEvent e, JLayer<? extends JComponent> l) {
-			super.processMouseWheelEvent(e, l);
+		private void reDraw() {
+			layer.setPreferredSize(new Dimension((int) (zoom * blackBox.getWidth()),
+					(int) (zoom * blackBox.getHeight())));
+
+			layer.revalidate();
+			layer.repaint();
 		}
 
-		public void zoomIn() {
-			if (5 - Math.abs(zoom) < 10e-5) {
-				return;
+		private AbstractAction zoomIn = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (5 - Math.abs(zoom) < 10e-5) {
+					return;
+				}
+
+				zoom *= 1.1;
+
+				Point point = MouseInfo.getPointerInfo().getLocation();
+				SwingUtilities.convertPointFromScreen(point, scrollPane);
+
+				Point pos = scrollPane.getViewport().getViewPosition();
+
+				int newX = (int) (point.x * (1.1f - 1f) + 1.1f * pos.x);
+				int newY = (int) (point.y * (1.1f - 1f) + 1.1f * pos.y);
+				scrollPane.getViewport().setViewPosition(new Point(newX, newY));
+
+				reDraw();
 			}
+		};
 
-			zoom += INCREMENT;
-		}
+		private AbstractAction zoomOut = new AbstractAction() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (Math.abs(zoom) - 0.25 < 10e-5) {
+					return;
+				}
 
-		public void zoomOut() {
-			if (Math.abs(zoom) - 0.25 < 10e-5) {
-				return;
+				zoom *= 0.9;
+
+				Point point = MouseInfo.getPointerInfo().getLocation();
+				SwingUtilities.convertPointFromScreen(point, scrollPane);
+
+				Point pos = scrollPane.getViewport().getViewPosition();
+
+				int newX = (int)(point.x*(0.9f - 1f) + 0.9f*pos.x);
+				int newY = (int)(point.y*(0.9f - 1f) + 0.9f*pos.y);
+				scrollPane.getViewport().setViewPosition(new Point(newX, newY));
+
+				reDraw();
 			}
-
-			zoom -= INCREMENT;
-		}
-
-		public double getZoom() {
-			return zoom;
-		}
+		};
 	}
 }
